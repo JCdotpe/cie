b3d027026bf99224dcb6a2e614860b2d3df0f0a0
Estefano Castañeda <mixc21@gmail.com>
2013-08-15 16:29:34 -0500

    update visor  view

--- application/views/visor/visor_view.php
+++ application/views/visor/visor_view.php
@@ -243,8 +243,8 @@
 
 		$('#c2').click(function(event) {
 			var code=$('.codigo_local').val();
-			/*get_P2_A(code);
-			get_P2_B(code);*/
+			get_P2_A(code);
+			get_P2_B(code);
 			get_P2_C(code);
 		});
 
