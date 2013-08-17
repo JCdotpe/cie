bd37a40b4f3fd9cc079355517742f7060c2696fd
miguel123456 <miguelsalinas.20@gmail.com>
2013-08-16 19:09:27 -0500

    updated

---
+++ application/views/visor/test.php
@@ -0,0 +1,249 @@
+
+
+<thead>
+		  	    					<tr>
+			  	    					<th colspan="3">Sección A: Identificación de las edificaciones</th>
+		  	    					</tr>
+		  	    				</thead>
+		  	    				<tbody>
+		  	    					<tr>
+		  	    						<td>2.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								Código de la edificación
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<span style="float:left;padding:8px;margin-right:10px; font-size:16px;" class="label label-default">E</span>
+		  	    							<input style="width:100px;float:left;" type="text" class="form-control">
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>3.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								Area techada del primer piso de la edificación
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							Area en m2
+		  	    							<table class="table table-bordered">
+		  	    								<thead>
+		  	    									<tr>
+		  	    										<th style="text-align:center">Enteros</th>
+		  	    										<th style="text-align:center">Decimales</th>
+		  	    									</tr>
+		  	    								</thead>
+		  	    								<tbody>
+		  	    									<tr>
+		  	    										<td></td>
+		  	    										<td></td>
+		  	    									</tr>
+		  	    								</tbody>
+		  	    							</table>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>4.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								Predio en el que se ubica la edificación
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label>Predio N°</label>
+		  	    							<input style="width:100px;float:left;" type="text" class="form-control">
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>5.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								¿esta edificación es parte del patrimonio cultural inmueble reconocido por el ministerio de cultura?
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label class="checkbox-inline">
+												<input type="radio" name="item1" id="inlineCheckbox2" value="option2"> 1. Si
+											</label>
+											<label class="checkbox-inline">
+												<input type="radio" name="item1" id="inlineCheckbox3" value="option3"> 2. No
+											</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>6.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								¿la edificación fue inspeccionada por defensa civil?
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label class="checkbox-inline">
+												<input type="radio" name="item2" id="inlineCheckbox2" value="option2"> 1. Si
+											</label>
+											<label class="checkbox-inline">
+												<input type="radio" name="item2" id="inlineCheckbox3" value="option3"> 2. No
+											</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>7.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								¿la edificación se encuentra declarada inhabitable (alto riesgo)?
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label class="checkbox-inline">
+												<input type="radio" name="item3" id="inlineCheckbox2" value="option2"> 1. Si
+											</label>
+											<label class="checkbox-inline">
+												<input type="radio" name="item3" id="inlineCheckbox3" value="option3"> 2. No
+											</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>8.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								Nº de pisos de esta edificación
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label>N° de pisos</label>
+		  	    							<input style="width:100px;float:left;" type="text" class="form-control">
+		  	    							<br /><br />
+		  	    							<label>Diligenciar las preguntas 8B, 8C, 8D según el número de pisos</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>8A.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								El primer piso cumple con la norma de accesibilidad para personas con discapacidad
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label class="checkbox-inline">
+												<input type="radio" name="radio1" id="inlineCheckbox2" value="option2"> 1. Si
+											</label>
+											<label class="checkbox-inline">
+												<input type="radio" name="radio1" id="inlineCheckbox3" value="option3"> 2. No
+											</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>8B.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								El segundo piso cumple con la norma de accesibilidad para personas con discapacidad
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label class="checkbox-inline">
+												<input type="radio" name="radio2" id="inlineCheckbox2" value="option2"> 1. Si
+											</label>
+											<label class="checkbox-inline">
+												<input type="radio" name="radio2" id="inlineCheckbox3" value="option3"> 2. No
+											</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>8C.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								El tercer piso cumple con la norma de accesibilidad para personas con discapacidad
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label class="checkbox-inline">
+												<input type="radio" name="radio3" id="inlineCheckbox2" value="option2"> 1. Si
+											</label>
+											<label class="checkbox-inline">
+												<input type="radio" name="radio3" id="inlineCheckbox3" value="option3"> 2. No
+											</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>8D.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								El cuarto piso cumple con la norma de accesibilidad para personas con discapacidad
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label class="checkbox-inline">
+												<input type="radio" name="radio4" id="inlineCheckbox2" value="option2"> 1. Si
+											</label>
+											<label class="checkbox-inline">
+												<input type="radio" name="radio4" id="inlineCheckbox3" value="option3"> 2. No
+											</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>9.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								¿cuántos niveles o modalidades hacen uso de esta edificación?
+		  	    							</strong>
+		  	    						</td>
+		  	    						<td>
+		  	    							<label>N° de niveles o modalidades</label>
+		  	    							<input style="width:100px;float:left;" type="text" class="form-control">
+		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<td>10.</td>
+		  	    						<td>
+		  	    							<strong>
+		  	    								¿los niveles o modalidades que hacen uso de esta edificación son:
+		  	    							</strong>
+		  	    							<br />(acepte uno o más códigos)
+		  	    						</td>
+		  	    						<td>
+		  	    							<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox2" value="option2"> 1. Inicial cuna?
+											</label>
+											<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox3" value="option3"> 2. Inicial jardín?
+											</label>
+		  	    							<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox2" value="option2"> 3. Inicial cuna jardín?
+											</label>
+											<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox3" value="option3"> 4. Primaria?
+											</label>
+		  	    							<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox2" value="option2"> 5. Secundaria?
+											</label>
+											<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox3" value="option3"> 6. Educación básica alternativa (eba)?
+											</label>
+		  	    							<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox2" value="option2"> 7. Educación básica especial (ebe)?
+											</label>
+											<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox3" value="option3"> 8. Educación superior de formación artistica (esfa)?
+											</label>
+		  	    							<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox2" value="option2"> 9. Instituto superior tecnológico (ist)?
+											</label>
+											<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox3" value="option3"> 10. Instituto superior pedagógico (isp)?
+											</label>
+		  	    							<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox2" value="option2"> 11. Centro  de educación técnico productivo (cetpro)?
+											</label>
+											<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox3" value="option3"> 12. Programa no escolarizado de educación inicial (pronoei)?
+											</label>
+		  	    							<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox2" value="option2"> 13. Sala de educación temprana?
+											</label>
+											<label class="checkbox">
+												<input type="checkbox" id="inlineCheckbox3" value="option3"> 14. Ludoteca?
+											</label>
+		  	    						</td>
+		  	    					</tr>
+		  	    				</tbody>
