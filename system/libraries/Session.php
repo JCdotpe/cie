5487207b02a74612a5d7a9d18b44e13f2436a044
miguel123456 <miguelsalinas.20@gmail.com>
2013-09-09 09:23:29 -0500

    updated session

--- system/libraries/Session.php
+++ system/libraries/Session.php
@@ -665,14 +665,14 @@ class CI_Session {
 		$expire = ($this->sess_expire_on_close === TRUE) ? 0 : $this->sess_expiration + time();
 
 		// Set the cookie
-		/*setcookie(
+		setcookie(
 					$this->sess_cookie_name,
 					$cookie_data,
 					$expire,
 					$this->cookie_path,
 					$this->cookie_domain,
 					$this->cookie_secure
-				);*/
+				);
 	}
 
 	// --------------------------------------------------------------------
