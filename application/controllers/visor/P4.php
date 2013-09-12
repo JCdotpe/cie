5c3d41dab06787b7563bf90ca846a277e327bc7e
JC <mrvixio@gmail.com>
2013-09-12 16:45:28 -0500

    4

--- application/controllers/visor/P4.php
+++ application/controllers/visor/P4.php
@@ -67,7 +67,7 @@ class P4 extends REST_Controller{
 
             header_json();
 
-            $data = $this->P4_model->getData($this->get('id_local'));
+            $data = $this->P4_model->getData(no_obfuscate($this->get('id_local')));
 
             $jsonData = json_encode($data->result());
 
