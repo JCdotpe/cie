651c2733666feec706e91777b6007950cdaefcd0
JC <mrvixio@gmail.com>
2013-09-12 19:11:17 -0500

    Cap 4 - finalizado falta foto

--- application/views/visor/capitulo4_view.php
+++ application/views/visor/capitulo4_view.php
@@ -58,39 +58,13 @@
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">N°</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">1A. Nº de tramo</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">1B. Longitud del tramo (m)</th>
-		  	    										<th style="text-align:center; vertical-align:middle;" colspan="2">1C. El tramo tiene cerco</th>
+		  	    										<th style="text-align:center; vertical-align:middle;" colspan="1">1C. El tramo tiene cerco</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">1D. El sistema estructural predominante del tramo es:</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">1E. Estado de conservación del tramo</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">1F. Opinión técnica del evaluador</th>
 		  	    									</tr>
-		  	    									<tr>
-		  	    										<th style="text-align:center; vertical-align:middle;"></th>
-		  	    										<th style="text-align:center; vertical-align:middle;"></th>
-		  	    									</tr>
 		  	    								</thead>
-		  	    								<tbody>
-		  	    									<tr>
-		  	    										<td style="text-align:center; vertical-align:middle;">1</td>
-		  	    										<td>Hasta 20</td>
-		  	    										<td></td>
-		  	    										<td>
-		  	    											<div class="checkbox">
-														      <label>
-														        <input type="checkbox"> 1
-														      </label>
-														    </div>
-		  	    										</td>
-		  	    										<td>
-		  	    											<div class="checkbox">
-														      <label>
-														        <input type="checkbox"> 2
-														      </label>
-														    </div>
-		  	    										</td>
-		  	    										<td></td>
-		  	    										<td></td>
-		  	    										<td></td>
-		  	    									</tr>
+		  	    								<tbody id="lindero_frente">
 		  	    								</tbody>
 		  	    							</table>
 
@@ -119,39 +93,13 @@
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">N°</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">2A. Nº de tramo</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">2B. Longitud del tramo (m)</th>
-		  	    										<th style="text-align:center; vertical-align:middle;" colspan="2">2C. El tramo tiene cerco</th>
+		  	    										<th style="text-align:center; vertical-align:middle;" colspan="1">2C. El tramo tiene cerco</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">2D. El sistema estructural predominante del tramo es:</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">2E. Estado de conservación del tramo</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">2F. Opinión técnica del evaluador</th>
 		  	    									</tr>
-		  	    									<tr>
-		  	    										<th style="text-align:center; vertical-align:middle;"></th>
-		  	    										<th style="text-align:center; vertical-align:middle;"></th>
-		  	    									</tr>
 		  	    								</thead>
-		  	    								<tbody>
-		  	    									<tr>
-		  	    										<td style="text-align:center; vertical-align:middle;">1</td>
-		  	    										<td>Hasta 20</td>
-		  	    										<td></td>
-		  	    										<td>
-		  	    											<div class="checkbox">
-														      <label>
-														        <input type="checkbox"> 1
-														      </label>
-														    </div>
-		  	    										</td>
-		  	    										<td>
-		  	    											<div class="checkbox">
-														      <label>
-														        <input type="checkbox"> 2
-														      </label>
-														    </div>
-		  	    										</td>
-		  	    										<td></td>
-		  	    										<td></td>
-		  	    										<td></td>
-		  	    									</tr>
+		  	    								<tbody id="lindero_derecho">
 		  	    								</tbody>
 		  	    							</table>
 
@@ -180,39 +128,13 @@
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">N°</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">3A. Nº de tramo</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">3B. Longitud del tramo (m)</th>
-		  	    										<th style="text-align:center; vertical-align:middle;" colspan="2">3C. El tramo tiene cerco</th>
+		  	    										<th style="text-align:center; vertical-align:middle;" colspan="1">3C. El tramo tiene cerco</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">3D. El sistema estructural predominante del tramo es:</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">3E. Estado de conservación del tramo</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">3F. Opinión técnica del evaluador</th>
 		  	    									</tr>
-		  	    									<tr>
-		  	    										<th style="text-align:center; vertical-align:middle;"></th>
-		  	    										<th style="text-align:center; vertical-align:middle;"></th>
-		  	    									</tr>
 		  	    								</thead>
-		  	    								<tbody>
-		  	    									<tr>
-		  	    										<td style="text-align:center; vertical-align:middle;">1</td>
-		  	    										<td>Hasta 20</td>
-		  	    										<td></td>
-		  	    										<td>
-		  	    											<div class="checkbox">
-														      <label>
-														        <input type="checkbox"> 1
-														      </label>
-														    </div>
-		  	    										</td>
-		  	    										<td>
-		  	    											<div class="checkbox">
-														      <label>
-														        <input type="checkbox"> 2
-														      </label>
-														    </div>
-		  	    										</td>
-		  	    										<td></td>
-		  	    										<td></td>
-		  	    										<td></td>
-		  	    									</tr>
+		  	    								<tbody id="lindero_fondo">
 		  	    								</tbody>
 		  	    							</table>
 
@@ -241,43 +163,21 @@
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">N°</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">4A. Nº de tramo</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">4B. Longitud del tramo (m)</th>
-		  	    										<th style="text-align:center; vertical-align:middle;" colspan="2">4C. El tramo tiene cerco</th>
+		  	    										<th style="text-align:center; vertical-align:middle;" colspan="1">4C. El tramo tiene cerco</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">4D. El sistema estructural predominante del tramo es:</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">4E. Estado de conservación del tramo</th>
 		  	    										<th style="text-align:center; vertical-align:middle;" rowspan="2">4F. Opinión técnica del evaluador</th>
 		  	    									</tr>
-		  	    									<tr>
-		  	    										<th style="text-align:center; vertical-align:middle;"></th>
-		  	    										<th style="text-align:center; vertical-align:middle;"></th>
-		  	    									</tr>
 		  	    								</thead>
-		  	    								<tbody>
-		  	    									<tr>
-		  	    										<td style="text-align:center; vertical-align:middle;">1</td>
-		  	    										<td>Hasta 20</td>
-		  	    										<td></td>
-		  	    										<td>
-		  	    											<div class="checkbox">
-														      <label>
-														        <input type="checkbox"> 1
-														      </label>
-														    </div>
-		  	    										</td>
-		  	    										<td>
-		  	    											<div class="checkbox">
-														      <label>
-														        <input type="checkbox"> 2
-														      </label>
-														    </div>
-		  	    										</td>
-		  	    										<td></td>
-		  	    										<td></td>
-		  	    										<td></td>
-		  	    									</tr>
+		  	    								<tbody id="lindero_izquierdo">
 		  	    								</tbody>
 		  	    							</table>
 
 		  	    						</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<th>OBSERVACIONES</th>
+		  	    						<td id="P4_1_Obs"></td>
 		  	    					</tr>
 
 		  	    				</tbody>
--- js/visor/capitulo4/index.js
+++ js/visor/capitulo4/index.js
@@ -15,52 +15,230 @@ $(document).ready(function(){
 function P4(){
 
 	$.getJSON(urlRoot('index.php')+'/visor/P4/Data/', {token: '7959ac60dc22523a9ac306ac6f9308d3d7201c56',id_local: localE(), predio: '1'}, function(data, textStatus) {
-
+		
 		$.each(data, function(index, val) {
 
 			$('#P4_1_Foto').val(val.P4_1_Foto);
-			$('#P4_1_Obs').val(val.P4_1_Obs);
+			$('#P4_1_Obs').html(val.P4_1_Obs);
+
 			$('#P4_2_CantTram_Lfrente').val(val.P4_2_CantTram_Lfrente);
 			$('#P4_2_CantTram_Lderecho').val(val.P4_2_CantTram_Lderecho);
 			$('#P4_2_CantTram_Lfondo').val(val.P4_2_CantTram_Lfondo);
-			$('#P4_2_CantTram_Lizq').val(val.P4_2_CantTram_Lfondo);
+			$('#P4_2_CantTram_Lizq').val(val.P4_2_CantTram_Lizq);
+
 
 		});
 
 
 	}).fail(function( jqxhr, textStatus, error ) {
-
+	
 		var err = textStatus + ', ' + error;
 		console.log( "Request Failed: " + err);
-
+	
 	});
-
+			
 }
 
 function P42N(){
 
+
+
 	$.getJSON(urlRoot('index.php')+'/visor/P42N/Data/', {token: '7959ac60dc22523a9ac306ac6f9308d3d7201c56',id_local: localE(), predio: '1'}, function(data, textStatus) {
 
-		$('#P4_2_LindTipo').val(val.P4_2_LindTipo);
-		$('#P4_2_1A_NroTramo').val(val.P4_2_1A_NroTramo);
-		$('#P4_2_1A_i').val(val.P4_2_1A_i);
-		$('#P4_2_1A_f').val(val.P4_2_1A_f);
-		$('#P4_2_1B_LongTramo').val(val.P4_2_1B_LongTramo);
-		$('#P4_2_1C_Cerco').val(val.P4_2_1C_Cerco);
-		$('#P4_2_1D_Estruc').val(val.P4_2_1D_Estruc);
-		$('#P4_2_1E_EstCons').val(val.P4_2_1E_EstCons);
-		$('#P4_2_1F_Opin').val(val.P4_2_1F_Opin);
+		i = 1;
 
+		var html_frente="";
+		var html_derecho="";
+		var html_fondo="";
+		var html_izquierdo="";
 
 		$.each(data, function(index, val) {
 
+			if(val.P4_2_1C_Cerco == 1)
+			{
+				text_P4_2_1C_Cerco = "SI";
+			}
+			else
+			{
+				text_P4_2_1C_Cerco = "NO";	
+			}
+
+			if(val.P4_2_1D_Estruc == 0)
+			{
+				text_P4_2_1D_Estruc = "";
+			}
+
+			if(val.P4_2_1D_Estruc == 1)
+			{
+				text_P4_2_1D_Estruc = "Albañilería confinada / armada";
+			}
+
+			if(val.P4_2_1D_Estruc == 2)
+			{
+				text_P4_2_1D_Estruc = "Albañilería sin confinar";
+			}			
+
+			if(val.P4_2_1D_Estruc == 3)
+			{
+				text_P4_2_1D_Estruc = "Malla electrosoldada / alambre";
+			}						
+
+			if(val.P4_2_1D_Estruc == 4)
+			{
+				text_P4_2_1D_Estruc = "Adobe / tapia / pirca u otros";
+			}									
+
+//
+			if(val.P4_2_1E_EstCons == 0)
+			{
+				text_P4_2_1E_EstCons = "";
+			}
+
+			if(val.P4_2_1E_EstCons == 1)
+			{
+				text_P4_2_1E_EstCons = "Sin daño";
+			}
+
+			if(val.P4_2_1E_EstCons == 2)
+			{
+				text_P4_2_1E_EstCons = "Fisuras leves";
+			}			
+
+			if(val.P4_2_1E_EstCons == 3)
+			{
+				text_P4_2_1E_EstCons = "Fisuras moderadas / ataque de sales";
+			}						
+
+			if(val.P4_2_1E_EstCons == 4)
+			{
+				text_P4_2_1E_EstCons = "Agrietamiento / colapso";
+			}									
+
+//
+
+//
+			if(val.P4_2_1F_Opin == 0)
+			{
+				text_P4_2_1F_Opin = "";
+			}
+
+			if(val.P4_2_1F_Opin == 1)
+			{
+				text_P4_2_1F_Opin = "Mantenimiento";
+			}
+
+			if(val.P4_2_1F_Opin == 2)
+			{
+				text_P4_2_1F_Opin = "Rehabilitación";
+			}			
+
+			if(val.P4_2_1F_Opin == 3)
+			{
+				text_P4_2_1F_Opin = "Demolición";
+			}						
+
+//
+
+
+// lindero frente
+
+			if(val.P4_2_LindTipo == 1)
+			{
+
+				html_frente+='<tr>'+
+					'<td>'+val.P4_2_1A_NroTramo+'</td>'+
+					'<td>'+val.P4_2_1A_i+' - '+val.P4_2_1A_f+'</td>'+
+					'<td>'+val.P4_2_1B_LongTramo+'</td>'+
+					'<td>'+text_P4_2_1C_Cerco+'</td>'+ 
+					'<td>'+text_P4_2_1D_Estruc+'</td>'+
+					'<td>'+text_P4_2_1E_EstCons+'</td>'+
+					'<td>'+text_P4_2_1F_Opin+'</td>'+
+				'</tr>';
+
+				$('#lindero_frente').html(html_frente);				
+
+			}
+
+// lindero frente
+
+// lindero_derecho
+
+			if(val.P4_2_LindTipo == 2)
+			{
+
+				html_derecho+='<tr>'+
+					'<td>'+val.P4_2_1A_NroTramo+'</td>'+
+					'<td>'+val.P4_2_1A_i+' - '+val.P4_2_1A_f+'</td>'+
+					'<td>'+val.P4_2_1B_LongTramo+'</td>'+
+					'<td>'+text_P4_2_1C_Cerco+'</td>'+
+					'<td>'+text_P4_2_1D_Estruc+'</td>'+
+					'<td>'+text_P4_2_1E_EstCons+'</td>'+
+					'<td>'+text_P4_2_1F_Opin+'</td>'+
+				'</tr>';
+
+				$('#lindero_derecho').html(html_derecho);				
+
+			}
+
+// lindero_derecho
+
+// lindero_fondo
+
+			if(val.P4_2_LindTipo == 3)
+			{
+
+				html_fondo+='<tr>'+
+					'<td>'+val.P4_2_1A_NroTramo+'</td>'+
+					'<td>'+val.P4_2_1A_i+' - '+val.P4_2_1A_f+'</td>'+
+					'<td>'+val.P4_2_1B_LongTramo+'</td>'+
+					'<td>'+text_P4_2_1C_Cerco+'</td>'+
+					'<td>'+text_P4_2_1D_Estruc+'</td>'+
+					'<td>'+text_P4_2_1E_EstCons+'</td>'+
+					'<td>'+text_P4_2_1F_Opin+'</td>'+
+				'</tr>';
+
+				$('#lindero_fondo').html(html_fondo);				
+
+			}
+
+// lindero_fondo
+
+// lindero_izquierdo
+
+			if(val.P4_2_LindTipo == 4)
+			{
+
+				html_izquierdo+='<tr>'+
+					'<td>'+val.P4_2_1A_NroTramo+'</td>'+
+					'<td>'+val.P4_2_1A_i+' - '+val.P4_2_1A_f+'</td>'+
+					'<td>'+val.P4_2_1B_LongTramo+'</td>'+
+					'<td>'+text_P4_2_1C_Cerco+'</td>'+
+					'<td>'+text_P4_2_1D_Estruc+'</td>'+
+					'<td>'+text_P4_2_1E_EstCons+'</td>'+
+					'<td>'+text_P4_2_1F_Opin+'</td>'+
+				'</tr>';
+
+				$('#lindero_izquierdo').html(html_izquierdo);				
+
+			}
+
+// lindero_izquierdo
+
+			i++;
+
 		});
 
-	}).fail(function( jqxhr, textStatus, error ) {
+					
 
+
+
+	}).fail(function( jqxhr, textStatus, error ) {
+  	
   		var err = textStatus + ', ' + error;
   		console.log( "Request Failed: " + err);
-
+	
 	});
+
+
 
 }
