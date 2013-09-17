a26c75555d5ce63e0473432905d4948f53b49c83
Estefano Castañeda <mixc21@gmail.com>
2013-09-17 15:27:14 -0500

    update maps

--- application/views/mapa/puntos_gps.php
+++ application/views/mapa/puntos_gps.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Simple Map</title>
+    <title>Puntos GPS de Locales Escolares</title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
     <style>
@@ -17,8 +17,21 @@
         padding: 8px;
       }
     </style>
+    <style type="text/css">
+       .labels {
+         color: black;
+         font-family: "Lucida Grande", "Arial", sans-serif;
+         font-size: 14px;
+         font-weight: bold;
+         text-align: center;
+         width: auto;
+         white-space: nowrap;
+       }
+   </style>
     
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <script src="<?php echo base_url('js/map/markerwithlabel.js'); ?>"></script>
+    <script src="<?php echo base_url('js/map/markcluster.js'); ?>"></script>
     <script>
 
     function initialize() {
@@ -57,18 +70,22 @@
 
     ?>
 
-          var marker<?php echo $x; ?> = new google.maps.Marker({
-            position: myLatlng<?php echo $x; ?>,
-            map: map,
-            title: <?php echo "'Local Nro. ".$filas->codigo_de_local."'"; ?>,
-            animation: google.maps.Animation.DROP
-          });
+         
 
-    
+           var marker<?php echo $x; ?> = new MarkerWithLabel({
+             position: myLatlng<?php echo $x; ?>,
+             draggable: true,
+             raiseOnDrag: true,
+             map: map,
+             labelContent: <?php echo "'".$filas->codigo_de_local." - ".$filas->nro_pred."'"; ?>,
+             labelAnchor: new google.maps.Point(22, 0),
+             labelClass: "labels", // the CSS class for the label
+             labelStyle: {opacity: 1.0}
+           });
 
        
           
-          var contentString<?php echo $x; ?>="<table style='margin-top:20px;' class='table table-bordered'>"+
+          var contentString<?php echo $x; ?>="<table>"+
             "<thead>"+
               "<tr>"+
               "<th colspan='9' style='text-align:center;'>Punto GPS</th>"+
@@ -98,15 +115,17 @@
             "</tbody>"+
           "</table>";
 
+
+
           var infowindow<?php echo $x; ?> = new google.maps.InfoWindow({
-            content: contentString<?php echo $x; ?>
+            content: contentString<?php echo $x; ?>,
           });
 
           google.maps.event.addListener(marker<?php echo $x; ?>, 'click', function() {
             infowindow<?php echo $x; ?>.open(map,marker<?php echo $x; ?>);
           });
 
-          
+          var markerCluster = new MarkerClusterer(map, marker<?php echo $x; ?>);
         <?php 
 
          }
@@ -116,6 +135,8 @@
      
 
       }
+
+
 
       google.maps.event.addDomListener(window, 'load', initialize);
 
