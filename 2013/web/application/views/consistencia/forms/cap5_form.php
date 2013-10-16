c6798d57a31b7abee1a3d68061426e6a50fbe6df
JC <mrvixio@gmail.com>
2013-10-16 11:32:28 -0500

    Cap 5 - Maquetacion OK

--- 2013/web/application/views/consistencia/forms/cap5_form.php
+++ 2013/web/application/views/consistencia/forms/cap5_form.php
@@ -12,55 +12,81 @@
 $P5_Tot_E = array(
 	'name'	=> 'P5_Tot_E',
 	'id'	=> 'P5_Tot_E',
+	'maxlength'	=> 2,
+	'class' => 'input2',		
 );
 
 $P5_Tot_P = array(
 	'name'	=> 'P5_Tot_P',
 	'id'	=> 'P5_Tot_P',
+	'maxlength'	=> 2,
+	'class' => 'input2',			
 );
 
 $P5_Tot_LD = array(
 	'name'	=> 'P5_Tot_LD',
 	'id'	=> 'P5_Tot_LD',
+	'maxlength'	=> 2,
+	'class' => 'input2',			
 );
 
 $P5_Tot_CTE = array(
 	'name'	=> 'P5_Tot_CTE',
 	'id'	=> 'P5_Tot_CTE',
+	'maxlength'	=> 2,
+	'class' => 'input2',			
 );
 
 $P5_Tot_MC = array(
 	'name'	=> 'P5_Tot_MC',
 	'id'	=> 'P5_Tot_MC',
+	'maxlength'	=> 2,
+	'class' => 'input2',			
 );
 
 $P5_Tot_L1 = array(
 	'name'	=> 'P5_Tot_L1',
 	'id'	=> 'P5_Tot_L1',
+	'maxlength'	=> 2,
+	'class' => 'input2',			
 );
 
 $P5_Tot_R = array(
 	'name'	=> 'P5_Tot_R',
 	'id'	=> 'P5_Tot_R',
+	'maxlength'	=> 2,
+	'class' => 'input2',			
 );
 
 $P5_Opin = array(
 	'name'	=> 'P5_Opin',
 	'id'	=> 'P5_Opin',
+	'maxlength'	=> 2,
+	'class' => 'input2',			
 );
 
 // FIN TABLA P5 - Capitulo V
 
 // TABLA P5_F - Capitulo V
 
+$P5_cantNroPiso = array(
+	'name'	=> 'P5_cantNroPiso',
+	'id'	=> 'P5_cantNroPiso',
+	'class' => 'input1',	
+);
+
 $P5_NroPiso = array(
 	'name'	=> 'P5_NroPiso',
 	'id'	=> 'P5_NroPiso',
+	'disabled' => 'disabled',
+	'class' => 'input1',	
 );
 
 $P5_Foto = array(
 	'name'	=> 'P5_Foto',
 	'id'	=> 'P5_Foto',
+	'disabled' => 'disabled',
+	'class' => 'input98p',
 );
 
 $P5_Escala = array(
@@ -80,6 +106,8 @@ $P5_Ed_Nro = array(
 $P5_TotAmb = array(
 	'name'	=> 'P5_TotAmb',
 	'id'	=> 'P5_TotAmb',
+	'maxlength'	=> 2,
+	'class' => 'input2',		
 );
 
 // TABLA P5_N - Capitulo V
@@ -96,54 +124,9 @@ echo '
 <div class="panel panel-info">
 	  	    				<div class="panel-heading">Capitulo V: Esquema de distribución de las edificaciones con ambientes</div>
 
-	  	    				<table class="table table-bordered">
-		  	    				<thead>
-		  	    					<tr>
-			  	    					<th>Piso N°1</th>
-		  	    					</tr>
-		  	    				</thead>
-		  	    				<tbody>
-		  	    					<tr>
-		  	    						<td id="piso_1"> </td>
-		  	    					</tr>
-		  	    				</tbody>
-		  	    			</table>
-	  	    				<table class="table table-bordered">
-		  	    				<thead>
-		  	    					<tr>
-			  	    					<th>Piso N°2</th>
-		  	    					</tr>
-		  	    				</thead>
-		  	    				<tbody>
-		  	    					<tr>
-		  	    						<td id="piso_2"></td>
-		  	    					</tr>
-		  	    				</tbody>
-		  	    			</table>
-	  	    				<table class="table table-bordered">
-		  	    				<thead>
-		  	    					<tr>
-			  	    					<th>Piso N°3</th>
-		  	    					</tr>
-		  	    				</thead>
-		  	    				<tbody>
-		  	    					<tr>
-		  	    						<td id="piso_3"></td>
-		  	    					</tr>
-		  	    				</tbody>
-		  	    			</table>
-	  	    				<table class="table table-bordered">
-		  	    				<thead>
-		  	    					<tr>
-			  	    					<th>Piso N°4</th>
-		  	    					</tr>
-		  	    				</thead>
-		  	    				<tbody>
-		  	    					<tr>
-		  	    						<td id="piso_4"></td>
-		  	    					</tr>
-		  	    				</tbody>
-		  	    			</table>
+
+	  	    				<h3>Resumen</h3>
+
 
 		  	    			<table class="table table-bordered">
 		  	    				<tbody><tr>
@@ -179,6 +162,28 @@ echo '
 		  	    					<td>'.form_input($P5_Opin).'</td>
 		  	    				</tr>			  	    				
 		  	    			</tbody></table>
+
+		  	    			<h3>Pisos</h3>	  	    				
+
+	  	    				<p>Número de pisos: '.form_input($P5_cantNroPiso).'</p>
+
+	  	    				<table class="table table-bordered">
+		  	    				<thead>
+		  	    					<tr>
+			  	    					<th colspan="2">Piso N° '.$P5_NroPiso.'</th>
+		  	    					</tr>
+		  	    				</thead>
+		  	    				<tbody>
+			  	    				<tr>
+		  	    						<td colspan="2">'.form_input($P5_Foto).'</td>
+		  	    					</tr>
+		  	    					<tr>
+		  	    						<th>Edificación N° '.$P5_Ed_Nro.'</th>
+		  	    						<td>Cantidad de Ambientes: '.form_input($P5_TotAmb).'</td>
+		  	    					</tr>		  	    					
+		  	    				</tbody>
+		  	    			</table>
+
 
 	  	    			</div>
 
