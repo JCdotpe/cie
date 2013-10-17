d194a7a955671425880c1236faaff32947cded4e
JC <mrvixio@gmail.com>
2013-10-17 12:57:10 -0500

    Cap 9 - Done

--- 2013/web/application/views/consistencia/forms/cap9_form.php
+++ 2013/web/application/views/consistencia/forms/cap9_form.php
@@ -1,1 +1,18 @@
-<h1>cap9</h1>
+<?php 
+
+$P9_F_Url_Foto = array(
+	'name'	=> 'P9_F_Url_Foto',
+	'id'	=> 'P9_F_Url_Foto',
+	'class'	=> 'input98p',
+	'disabled' => 'disabled',
+);
+
+echo '
+
+<h3>Lista de fotos en Base de Datos: </h3>
+
+'.form_input($P9_F_Url_Foto).'
+
+';
+
+?>
