2520445271d3ef3ba46b5c719e31b6b80db96ac0
Flexcash <puraficha@gmail.com>
2013-11-04 17:02:02 -0500

    cap1 fix

--- 2013/web/application/views/consistencia/forms/cap1_form.php
+++ 2013/web/application/views/consistencia/forms/cap1_form.php
@@ -293,7 +293,7 @@ $P1_B_3_2_AntReg_Cod = array(
 $P1_B_3_3_AntReg_Nro = array(
 	'name'	=> 'P1_B_3_3_AntReg_Nro',
 	'id'	=> 'P1_B_3_3_AntReg_Nro',
-	'maxlength'	=> 1,
+	'maxlength'	=> 20,
 );
 
 $P1_B_3_4_Tipo_TProp = array(
@@ -306,7 +306,7 @@ $P1_B_3_4_Tipo_TProp = array(
 $P1_B_3_4_Tipo_TProp_O = array(
 	'name'	=> 'P1_B_3_4_Tipo_TProp_O',
 	'id'	=> 'P1_B_3_4_Tipo_TProp_O',
-	'maxlength'	=> 1,
+	'maxlength'	=> 255,
 );
 
 $P1_B_3_5_FecTit = array(
