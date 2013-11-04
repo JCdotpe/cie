bc8835b2ce19f1dd6bec66598c3f46507451f68d
Flexcash <puraficha@gmail.com>
2013-11-04 10:53:20 -0500

    asd

--- 2013/web/application/views/consistencia/forms/car_form.php
+++ 2013/web/application/views/consistencia/forms/car_form.php
@@ -735,6 +735,32 @@ $(function(){
 //datepicker
 
 
+// $(document).on("change",'.car_res',function() {
+// 		var pre = $(this).attr('id');
+// 		var tex = pre.substring(0,14);
+// 		var nro;
+
+// 		if(pre.length == 15){
+// 		 	nro = pre.substring(14,15);
+// 		}
+// 		else if(pre.length == 16){
+// 			nro = pre.substring(14,16);
+// 		}			
+
+// 	if($(this).val() == 5){
+// 		 if(tex == 'PC_C_1_Et_Res_')
+// 		 	$('#PC_C_1_Et_Res_O_' + nro).removeAttr('disabled');
+// 		 else if(tex == 'PC_C_1_Jb_Res_')
+// 		 	$('#PC_C_1_Jb_Res_O_' + nro).removeAttr('disabled');
+// 	}else{
+// 		 if(tex == 'PC_C_1_Et_Res_')
+// 		 	$('#PC_C_1_Et_Res_O_' + nro).attr('disabled','disabled');
+// 		 else if(tex == 'PC_C_1_Jb_Res_')
+// 		 	$('#PC_C_1_Jb_Res_O_' + nro).attr('disabled','disabled');	
+// 	}
+
+// });
+
 
 //car
 $("#PC_A_1_Dep").change(function(event) {
@@ -844,11 +870,11 @@ $('#pcar_c_n tr').remove('.entrev');
 	    asd +='<td><input type="text" class="input5 embc' + i + '" maxlength="5" name="PC_C_1_Et_Hfin[]" id="PC_C_1_Et_Hfin_' + i + '" value="" ><div class="help-block error"></div></td>';
 	    asd +='<td><input type="text" class="input10 embc' + i + ' fechap" readonly maxlength="10" name="PC_C_1_Et_Fecha_Prox[]" id="PC_C_1_Et_Fecha_Prox_' + i + '" value="" ><div class="help-block error"></div></td>';
 	    asd +='<td><input type="text" class="input5 embc' + i + '" maxlength="5" name="PC_C_1_Et_Hora_Prox[]" id="PC_C_1_Et_Hora_Prox_' + i + '" value="" ><div class="help-block error"></div></td>';
-	    asd +='<td><input type="text" class="input1 embc' + i + '" maxlength="1" name="PC_C_1_Et_Res[]" id="PC_C_1_Et_Res_' + i + '" value="" > - Especifique <div class="help-block error"><input type="text" class="input10 embc' + i + '" maxlength="80" name="PC_C_1_Et_Res_O' + '_' + i + '" id="PC_C_1_Et_Res_O' + '_' + i + '" value="" ><div class="help-block error"></div></td>';
-	    asd +='<td><input type="text" class="input10 embc' + i + '" maxlength="10" name="PC_C_1_Jb_Fecha[]" id="PC_C_1_Jb_Fecha_' + i + '" value="" ><div class="help-block error"></div></td>';
+	    asd +='<td><input type="text" class="input1 embc' + i + ' car_res" maxlength="1" name="PC_C_1_Et_Res[]" id="PC_C_1_Et_Res_' + i + '" value="" > - Especifique <div class="help-block error"></div><input type="text" class="input10 embc' + i + '"  maxlength="80" name="PC_C_1_Et_Res_O[]" id="PC_C_1_Et_Res_O' + '_' + i + '" value="" ><div class="help-block error"></div></td>';
+	    asd +='<td><input type="text" class="input10 embc' + i + ' fechap" readonly maxlength="10" name="PC_C_1_Jb_Fecha[]" id="PC_C_1_Jb_Fecha_' + i + '" value="" ><div class="help-block error"></div></td>';
 	    asd +='<td><input type="text" class="input5 embe' + i + '" maxlength="5" name="PC_C_1_Jb_Hini[]" id="PC_C_1_Jb_Hini_' + i + '" value="" ><div class="help-block error"></div></td>';
 	    asd +='<td><input type="text" class="input5 embe' + i + '" maxlength="5" name="PC_C_1_Jb_Hfin[]" id="PC_C_1_Jb_Hfin_' + i + '" value="" ><div class="help-block error"></div></td>';
-	    asd +='<td><input type="text" class="input1 embc' + i + '" maxlength="1" name="PC_C_1_Jb_Res[]" id="PC_C_1_Jb_Res_' + i + '" value="" > - Especifique <div class="help-block error"><input type="text" class="input10 embc' + i + '" maxlength="80" name="PC_C_1_Jb_Res_O' + '_' + i + '" id="PC_C_1_Jb_Res_O' + '_' + i + '" value="" ></div></td>';
+	    asd +='<td><input type="text" class="input1 embc' + i + ' car_res" maxlength="1" name="PC_C_1_Jb_Res[]" id="PC_C_1_Jb_Res_' + i + '" value="" > - Especifique <div class="help-block error"></div><input type="text" class="input10 embc' + i + '"  maxlength="80" name="PC_C_1_Jb_Res_O[]" id="PC_C_1_Jb_Res_O' + '_' + i + '" value="" ></div></td>';
 	    asd += '</tr>';
 	    $('#pcar_c_n > tbody').append(asd);
 	  }
@@ -908,7 +934,18 @@ $("#car_f").validate({
 			    },	       
 			    'PC_C_1_Et_Fecha_Prox[]':{
 			    	required:true,
-			    },  		                  	         		         	         	          		                                                                             
+			    },  		              
+			    'PC_C_1_Et_Res[]':{
+			    		range: [1,5],
+			    		required: true,			    	
+			    },    	   
+			    'PC_C_1_Jb_Res[]':{
+			    		range: [1,5],
+			    		// required: true,			    	
+			    },          
+			    'PC_C_1_Et_Res_O[]'	:{
+			    		required: true,			    	
+			    },          	         	         	          		                                                                          
 				PC_D_EvT_dni: {
 			    		digits: true,
 			    		required: true,
