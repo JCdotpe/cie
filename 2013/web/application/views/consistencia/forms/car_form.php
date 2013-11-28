ce96dc79096473d57f6fcea9289da87e5e858572
Flexcash <puraficha@gmail.com>
2013-11-28 09:53:01 -0500

    vals car

--- 2013/web/application/views/consistencia/forms/car_form.php
+++ 2013/web/application/views/consistencia/forms/car_form.php
@@ -432,8 +432,8 @@ echo '
 									<li class="list-group-item"><div style="width:150px; margin-left:10px; float:left;">2. Provincia </div> '. form_dropdown('PC_A_2_Prov', $provArray, FALSE,'class="input200" id="PC_A_2_Prov"') .' <div class="help-block error"></div></li>
 									<li class="list-group-item"><div style="width:150px; margin-left:10px; float:left;">3. Distrito </div> '. form_dropdown('PC_A_3_Dist', $distArray, FALSE,'class="input200" id="PC_A_3_Dist"') .' <div class="help-block error"></div></li>
 									<li class="list-group-item"><div style="width:150px; margin-left:10px; float:left;">4. Centro Poblado </div> '.form_input($PC_A_4_CentroP).'<div class="help-block error"></div></li>
-									<li class="list-group-item"><div style="width:150px; margin-left:10px; float:left;">5. Nucleo Urbano </div> '.form_input($PC_A_5_NucleoUrb).'</li>
-									<li class="list-group-item"><div style="width:150px; margin-left:10px; float:left;">6. UGEL </div> '.form_input($PC_A_6_Ugel).'</li>
+									<li class="list-group-item"><div style="width:150px; margin-left:10px; float:left;">5. Nucleo Urbano </div> '.form_input($PC_A_5_NucleoUrb).' <div class="help-block error"></div></li>
+									<li class="list-group-item"><div style="width:150px; margin-left:10px; float:left;">6. UGEL </div> '.form_input($PC_A_6_Ugel).' <div class="help-block error"></div></li>
 								</ul>
 
 						</div>
