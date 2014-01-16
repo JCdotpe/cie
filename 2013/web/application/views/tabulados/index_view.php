668337776913765165549160b5fd5e269f20d60c
Jhonatan Rivera <yonatan.rc@gmail.com>
2014-01-16 10:08:02 -0500

    tabulados index

--- 2013/web/application/views/tabulados/index_view.php
+++ 2013/web/application/views/tabulados/index_view.php
@@ -7,7 +7,7 @@
           <div class="well sidebar-nav cen-sidebar">
             <ul class="nav nav-list">
               <li class="nav-header">OPCIONES</li>
-              <li ><a href="<?php echo site_url('tabulados/capitulo7/reporte29'); ?>">Tabulados Pescador</a></li>
+              <li ><a href="<?php echo site_url('tabulados/capitulo7/reporte29'); ?>">Tabulados Capitulo 7</a></li>
               <li ><a href="<?php echo site_url('tabulados/referenciacion'); ?>">GEOREFERENCIACION</a></li> 
           </div>
   </div>
